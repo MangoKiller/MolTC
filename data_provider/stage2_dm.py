cdacaed04e1535b920b5c16e36d69953e75a4ea4
JunfengFang <46044330+MangoKiller@users.noreply.github.com>
2024-07-14 19:42:52 +0800

    Update stage2_dm.py

--- data_provider/stage2_dm.py
+++ data_provider/stage2_dm.py
@@ -6,7 +6,7 @@ import torch_geometric
 # from torch_geometric.loader import DataLoader
 from torch.utils.data import DataLoader
 from torch_geometric.loader.dataloader import Collater
-from data_provider.molecule_caption_dataset import MoleculeCaption, MoleculeCaption_double,MoleculeCaption_double_value,MoleculeCaption_double_DDIvalue,MoleculeCaption_double_fgtvalue
+from data_provider.molecule_caption_dataset import MoleculeCaption, MoleculeCaption_double,MoleculeCaption_double_value,MoleculeCaption_double_DDIvalue,MoleculeCaption_double_fgtvalue,MoleculeCaption_universal
 import re
 
 # we split individual characters inside special tokens like [START_DNA]
@@ -144,6 +144,7 @@ class TrainCollater_double:
 
         is_mol_token = smiles_prompt_tokens.input_ids == self.mol_token_id
         smiles_prompt_tokens['is_mol_token'] = is_mol_token
+
         #print(texts1)
         text_tokens = self.tokenizer(text=texts,
                                      truncation=True,
@@ -253,10 +254,10 @@ class Stage2DM(LightningDataModule):
         self.num_workers = num_workers
         self.text_max_len = text_max_len
         self.prompt = args.prompt
-        self.pretrain_dataset = MoleculeCaption(root+f'/pretrain/', text_max_len, self.prompt)
-        self.train_dataset = MoleculeCaption(root+f'/train/', text_max_len, self.prompt)
-        self.val_dataset = MoleculeCaption(root + '/valid/', text_max_len, self.prompt)
-        self.test_dataset = MoleculeCaption(root + '/test/', text_max_len, self.prompt)
+        self.pretrain_dataset = MoleculeCaption('qformer_data/train/', text_max_len, self.prompt)
+        self.train_dataset = MoleculeCaption('qformer_data/train/', text_max_len, self.prompt)
+        self.val_dataset = MoleculeCaption('qformer_data/val/', text_max_len, self.prompt)
+        self.test_dataset = MoleculeCaption('qformer_data/val/', text_max_len, self.prompt)
         self.init_tokenizer(tokenizer)
         self.mol_ph_token = '<mol>' * self.args.num_query_token
         
@@ -378,8 +379,8 @@ class Stage2DM_double(LightningDataModule):
 
         self.pretrain_dataset =  MoleculeCaption_double(root, text_max_len, self.prompt)
         self.train_dataset =  MoleculeCaption_double(root, text_max_len, self.prompt)
-        self.val_dataset =  MoleculeCaption_double(args.valid_root, text_max_len, self.prompt)
-        self.test_dataset = MoleculeCaption_double(args.valid_root, text_max_len, self.prompt)
+        self.val_dataset =  MoleculeCaption_double('qformer_data/val/', text_max_len, self.prompt)
+        self.test_dataset = MoleculeCaption_double('qformer_data/val/', text_max_len, self.prompt)
         self.init_tokenizer(tokenizer)
         self.mol_ph_token = '<mol>' * self.args.num_query_token
         
@@ -754,8 +755,8 @@ class Stage2DM_double_fgtvalue(LightningDataModule):
         self.prompt = args.prompt
         self.pretrain_dataset =  MoleculeCaption_double_fgtvalue(root, text_max_len, self.prompt)
         self.train_dataset =  MoleculeCaption_double_fgtvalue(root, text_max_len, self.prompt)
-        self.val_dataset =  MoleculeCaption_double_fgtvalue(args.valid_root, text_max_len, self.prompt)
-        self.test_dataset = MoleculeCaption_double_fgtvalue(args.valid_root, text_max_len, self.prompt)
+        self.val_dataset =  MoleculeCaption_double_fgtvalue(valid_root, text_max_len, self.prompt)
+        self.test_dataset = MoleculeCaption_double_fgtvalue(valid_root, text_max_len, self.prompt)
         self.init_tokenizer(tokenizer)
         self.mol_ph_token = '<mol>' * self.args.num_query_token
         
@@ -784,6 +785,133 @@ class Stage2DM_double_fgtvalue(LightningDataModule):
             )
         elif self.mode == 'ft':
             print(self.pretrain_dataset)
+            loader = DataLoader(
+                self.train_dataset,
+                batch_size=self.batch_size,
+                shuffle=True,
+                num_workers=self.num_workers,
+                pin_memory=False,
+                drop_last=True,
+                persistent_workers=True,
+                collate_fn=TrainCollater_double(self.tokenizer, self.text_max_len, self.mol_ph_token, self.mol_token_id),
+            )
+        else:
+            raise NotImplementedError
+        return loader
+
+    # def val_dataloader(self):
+    #     loader = DataLoader(
+    #         self.val_dataset,
+    #         batch_size=self.batch_size,
+    #         shuffle=False,
+    #         num_workers=self.num_workers,
+    #         pin_memory=False,
+    #         drop_last=False,
+    #         persistent_workers=True,
+    #         collate_fn=TrainCollater(self.tokenizer, self.text_max_len),
+    #     )
+    #     return [loader,]
+    
+    def val_dataloader(self):
+        val_loader = DataLoader(
+            self.val_dataset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=self.num_workers,
+            pin_memory=False,
+            drop_last=False,
+            persistent_workers=True,
+            collate_fn=TrainCollater_double(self.tokenizer, self.text_max_len, self.mol_ph_token, self.mol_token_id),
+        )
+        test_loader = DataLoader(
+            self.test_dataset,
+            batch_size=self.inference_batch_size,
+            shuffle=False,
+            num_workers=self.num_workers,
+            pin_memory=False,
+            drop_last=False,
+            persistent_workers=True,
+            #注意 在这里和下面我把测试集数据也都改了
+            collate_fn=InferenceCollater_double(self.tokenizer, self.text_max_len, self.mol_ph_token, self.mol_token_id),
+        )
+        return [val_loader, test_loader]
+    
+    def test_dataloader(self):
+        loader = DataLoader(
+            self.test_dataset,
+            batch_size=self.inference_batch_size,
+            shuffle=False,
+            num_workers=self.num_workers,
+            pin_memory=False,
+            drop_last=False,
+            persistent_workers=True,
+            collate_fn=InferenceCollater_double(self.tokenizer, self.text_max_len, self.mol_ph_token, self.mol_token_id),
+        )
+        return loader
+
+    def add_model_specific_args(parent_parser):
+        parser = parent_parser.add_argument_group("Data module")
+        parser.add_argument('--num_workers', type=int, default=2)
+        parser.add_argument('--batch_size', type=int, default=32)
+        parser.add_argument('--inference_batch_size', type=int, default=4)
+        parser.add_argument('--use_smiles', action='store_true', default=False)
+        parser.add_argument('--root', type=str, default='data/PubChemDataset_v4')
+        parser.add_argument('--text_max_len', type=int, default=128)
+        parser.add_argument('--prompt', type=str, default='The SMILES of this molecule is [START_I_SMILES]{}[END_I_SMILES]. ')
+        return parent_parser
+
+
+
+class Stage2DM_universal(LightningDataModule):
+    def __init__(
+        self,
+        mode: str = 'pretrain',
+        num_workers: int = 0,
+        batch_size: int = 256,
+        root: str = 'data/',
+        text_max_len: int = 128,
+        tokenizer=None,
+        args=None,
+    ):
+        super().__init__()
+        self.args = args
+        self.mode = mode
+        self.batch_size = batch_size
+        self.inference_batch_size = args.inference_batch_size
+        self.num_workers = num_workers
+        self.text_max_len = text_max_len
+        self.prompt = args.prompt
+        self.pretrain_dataset =  MoleculeCaption_universal(root, text_max_len, self.prompt)
+        self.train_dataset =  MoleculeCaption_universal(root, text_max_len, self.prompt)
+        self.val_dataset =  MoleculeCaption_universal(self.args.valid_root, text_max_len, self.prompt)
+        self.test_dataset = MoleculeCaption_universal(self.args.valid_root, text_max_len, self.prompt)
+        self.init_tokenizer(tokenizer)
+        self.mol_ph_token = '<mol>' * self.args.num_query_token
+        
+    
+    def init_tokenizer(self, tokenizer):
+        self.tokenizer = tokenizer
+        self.pretrain_dataset.tokenizer = tokenizer
+        self.train_dataset.tokenizer = tokenizer
+        self.val_dataset.tokenizer = tokenizer
+        self.test_dataset.tokenizer = tokenizer
+        self.mol_token_id = self.tokenizer.mol_token_id
+        # self.tokenizer.mol_token_id = tokenizer("<mol>", add_special_tokens=False).input_ids[0]
+
+    def train_dataloader(self):
+        if self.mode == 'pretrain':
+            print(self.pretrain_dataset)
+            loader = DataLoader(
+                self.pretrain_dataset,
+                batch_size=self.batch_size,
+                shuffle=True,
+                num_workers=self.num_workers,
+                pin_memory=False,
+                drop_last=True,
+                persistent_workers=True,
+                collate_fn=TrainCollater_double(self.tokenizer, self.text_max_len, self.mol_ph_token, self.mol_token_id),
+            )
+        elif self.mode == 'ft':
             loader = DataLoader(
                 self.train_dataset,
                 batch_size=self.batch_size,
